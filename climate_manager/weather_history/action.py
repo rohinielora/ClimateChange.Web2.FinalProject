b38b078490d77a1b6f8cbb8d24191f2a90febae9
rohinielora <70495118+rohinielora@users.noreply.github.com>
2024-04-21 16:03:01 -0400

    Update action.py

--- climate_manager/weather_history/action.py
+++ climate_manager/weather_history/action.py
@@ -2,3 +2,12 @@ from climate_manager.models.location import Location
 
 def create_location_weather_record(location, date, temperature, precipitation):
     return WeatherRecord(location, date, temperature, precipitation)
+""" recently added """
+
+
+def rank_countries_by_temperature():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT country, temperature FROM weather ORDER BY temperature DESC")
+    ranked_countries = cursor.fetchall()
+    return ranked_countries
